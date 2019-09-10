5bbba9d929dc731281ee348e802f89b257272149
Shishak <semenshishak@gmail.com>
2019-09-10 03:00:24 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: `@progital/gatsby-theme-wp-source-one`,
       options: {
-        wordPressUrl: `http://3.121.214.216.xip.io/`,
+        wordPressUrl: `http://18.204.169.216/`,
         processPostTypes: [`Page`, `Post`],
         useWebp: true,
       },
